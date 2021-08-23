0e4914fc02232886dab2e715e94f8b3e9158556f
Gavin Macaulay <gavin@macaulay.co.nz>
2021-08-23 18:54:53 +1200

    Version that works in final form of hand controller
    - changed ports that the switches use
    - changed from encoder to potentiometer for speed control

--- controller/main.py
+++ controller/main.py
@@ -6,27 +6,19 @@ import xbee
 import sys
 
 # Pin definitions
-winch1out = machine.Pin(machine.Pin.board.D5, machine.Pin.IN, machine.Pin.PULL_UP)
+winch1out = machine.Pin(machine.Pin.board.D12, machine.Pin.IN, machine.Pin.PULL_UP)
 winch1in = machine.Pin(machine.Pin.board.D10, machine.Pin.IN, machine.Pin.PULL_UP)
 
-winch2out = machine.Pin(machine.Pin.board.D3, machine.Pin.IN, machine.Pin.PULL_UP)
-winch2in = machine.Pin(machine.Pin.board.D2, machine.Pin.IN, machine.Pin.PULL_UP)
+winch2out = machine.Pin(machine.Pin.board.D11, machine.Pin.IN, machine.Pin.PULL_UP) # D18
+winch2in = machine.Pin(machine.Pin.board.D1, machine.Pin.IN, machine.Pin.PULL_UP) # D16
 
-winch3out = machine.Pin(machine.Pin.board.D11, machine.Pin.IN, machine.Pin.PULL_UP)
-winch3in = machine.Pin(machine.Pin.board.D1, machine.Pin.IN, machine.Pin.PULL_UP)
+winch3out = machine.Pin(machine.Pin.board.D3, machine.Pin.IN, machine.Pin.PULL_UP)
+winch3in = machine.Pin(machine.Pin.board.D2, machine.Pin.IN, machine.Pin.PULL_UP)
 
-speedBit0 = machine.Pin(machine.Pin.board.D12, machine.Pin.IN, machine.Pin.PULL_UP)
-speedBit1 = machine.Pin(machine.Pin.board.D9, machine.Pin.IN, machine.Pin.PULL_UP)
-speedBit2 = machine.Pin(machine.Pin.board.D0, machine.Pin.IN, machine.Pin.PULL_UP)
-#speedBit3 = machine.Pin(machine.Pin.board.XXX, machine.Pin.IN, machine.Pin.PULL_UP) # not used
+speedPot = machine.ADC(machine.Pin.board.D0)
 
 led = machine.Pin(machine.Pin.board.D4, machine.Pin.OUT)
 ledState = False
-
-# Table to convert the binary value from the encoder into a speed value
-#speed = [6, 5, 4, 3, 2, 1, 0, 7] 
-#speed = [223, 191, 159, 127, 95, 63, 0, 255]
-speed = [191, 159, 127, 95, 63, 31, 0, 255]
 
 # Time between checking controls (also the time between sending messages to the winches)
 pollInterval = 200 # [ms] 
@@ -61,10 +53,14 @@ while True:
                 s += up
             else:
                 s += stop
-                
-            encoderValue = speedBit0.value() + 2*speedBit1.value() + 4*speedBit2.value()
-            #s += str(speed[encoderValue])
-            s += '{:03d}'.format(speed[encoderValue])
+
+            # The ADC is 12 bit, but we only want 8 bits to send to the winches,
+            # so chop off the lower bits (and remove some ADC noise too)                
+            speed = speedPot.read()
+
+            s += '{:03d}'.format(speed >> 4)
+            
+            print(s)
             
             # Send to whoever is listening
             xbee.transmit(xbee.ADDR_BROADCAST, s)
