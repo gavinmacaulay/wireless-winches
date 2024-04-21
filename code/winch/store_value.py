b52b19c4163414077aa12a4660608e07ce1276bb
Gavin Macaulay <gavin@macaulay.co.nz>
2024-04-21 13:31:25 +1200

    Fix some bugs

--- code/winch/store_value.py
+++ code/winch/store_value.py
@@ -35,18 +35,18 @@ class storeValue():
         self.cPos = next(self.posAlternator)  # get next location to store at
         ba = bytearray(pack(self.storeFormat, v))  # make bytearray of data
         _ = self.f.readwrite(self.cPos+1, ba, read=False)  # store data
-        f[self.cPos] = self.__checksum(ba)  # store checksum
+        self.f[self.cPos] = self.__checksum(ba)  # store checksum
         # Store location of most recently written data. If this line succeeds, then
         # all is good. If it fails (e.g. due to loss of power), a read will get the 
         # data in the previous location, which should be good.
-        f[0] = self.cPos  # store location
+        self.f[0] = self.cPos  # store location
 
     def get(self):
         """Retrieve a value."""
-        cPos = f[0] # get location of most recently written data
-        checksum = f[cPos]
+        cPos = self.f[0] # get location of most recently written data
+        checksum = self.f[cPos]
         ba = bytearray(self.storeSize)
-        _ = f.readwrite(cPos+1, ba, read=True)
+        _ = self.f.readwrite(cPos+1, ba, read=True)
         v, = unpack(self.storeFormat, ba)
         if not (checksum == self.__checksum(ba)):
             # should never happen
