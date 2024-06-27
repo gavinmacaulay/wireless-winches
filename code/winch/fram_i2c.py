8ee8ae3dd338efd0d02c6e77803f77c867b42ec7
Gavin Macaulay <gavin@macaulay.co.nz>
2024-06-28 10:57:06 +1200

    Fix more lint warnings

--- code/winch/fram_i2c.py
+++ code/winch/fram_i2c.py
@@ -1,17 +1,21 @@
-# fram_i2c.py Driver for Fujitsu 16K Ferroelectric RAM module (Fujitsu MB85RC16)
+"""Driver for Fujitsu 16K Ferroelectric RAM module (Fujitsu MB85RC16)."""
 
 from micropython import const
 
 _SIZE = const(2048)  # Chip size [bytes]
 _ADDR = const(0x50)  # FRAM I2C address 0x50 to 0x57
 
+
 class FRAM():
+    """Class to communicate with the FRAM."""
+
     def __init__(self, i2c):
         self._i2c = i2c
         self.lsb = bytearray(1)
         self.i2c_addr = bytearray(1)
 
     def scan(self, verbose=False):
+        """Scan the I2C bus for FRAM chips."""
         devices = self._i2c.scan()
         chips = [d for d in devices if d == _ADDR]
         if len(chips) == 0:
@@ -21,15 +25,16 @@ class FRAM():
         return chips
 
     def readwrite(self, loc, buf, read):
+        """Read or write to the FRAM."""
         mvb = memoryview(buf)
         if len(buf) > 0:
-            msb = (loc << 5) >> 13 # top 3 bits of lowest 11 bits
-            self.lsb[0] = loc & 0xFF # lowest 8 bits of 11 bits
-            self.i2c_addr[0] = _ADDR + msb # I2C address (7 bits)
+            msb = (loc << 5) >> 13  # top 3 bits of lowest 11 bits
+            self.lsb[0] = loc & 0xFF  # lowest 8 bits of 11 bits
+            self.i2c_addr[0] = _ADDR + msb  # I2C address (7 bits)
 
             if read:
                 self._i2c.writeto(self.i2c_addr[0], self.lsb)
                 self._i2c.readfrom_into(self.i2c_addr[0], mvb)
-            else: # write
+            else:  # write
                 self._i2c.writevto(self.i2c_addr[0], (self.lsb, mvb))
         return buf
--- code/winch/store_value.py
+++ code/winch/store_value.py
@@ -1,26 +1,28 @@
+"""Store data in FRAM."""
+
 from machine import I2C
 from fram_i2c import FRAM
 from struct import pack, unpack, calcsize
 from micropython import const
 
+
 class storeValue():
-    """
-    Stores a single value in FRAM with checksum.
-    
+    """Store a single value in FRAM with checksum.
+
     Alternates between two storage locations so that if a store fails due
     to power failure during write, the previous storage location can be used
     to get a value.
-    
+
     Memory is used thus:
     byte, usage
     0,    latest pos index
-    1,    checksum 
+    1,    checksum
     2-5,  data
     6,    checksum
     7-10, data
-    
+
     """
-    
+
     def __init__(self):
         # Setup access to the FRAM
         self.i2c = I2C(1, freq=400000)
@@ -28,10 +30,10 @@ class storeValue():
         self.posAlternator = self.__storeLocation()
         self.checksum = bytearray(1)
         self.cPos = bytearray(1)
-        
+
         self.storeFormat = const('@f')  # will be used in pack() and unpack()
-        self.storeSize = const(calcsize(self.storeFormat))  # bytes 
-                    
+        self.storeSize = const(calcsize(self.storeFormat))  # bytes
+
     def put(self, v):
         """Store the given value."""
         self.cPos[0] = next(self.posAlternator)  # get next location to store at
@@ -41,17 +43,17 @@ class storeValue():
         self.f.readwrite(self.cPos[0], self.checksum, read=False)  # store checksum
 
         # Store location of most recently written data. If this line succeeds, then
-        # all is good. If it fails (e.g. due to loss of power), a read will get the 
+        # all is good. If it fails (e.g. due to loss of power), a read will get the
         # data in the previous location, which should be good.
         self.f.readwrite(0, self.cPos, read=False)  # store location
 
     def get(self):
         """Retrieve a value."""
-        self.f.readwrite(0, self.cPos, read=True) # get location of most recently written data
-        self.f.readwrite(self.cPos[0], self.checksum, read=True) # get checksum
-        
+        self.f.readwrite(0, self.cPos, read=True)  # get location of most recently written data
+        self.f.readwrite(self.cPos[0], self.checksum, read=True)  # get checksum
+
         ba = bytearray(self.storeSize)
-        _ = self.f.readwrite(self.cPos[0]+1, ba, read=True) # get value
+        _ = self.f.readwrite(self.cPos[0]+1, ba, read=True)  # get value
 
         v, = unpack(self.storeFormat, ba)
 
@@ -59,16 +61,16 @@ class storeValue():
             # should never happen
             raise RuntimeError('Could not read a valid value from FRAM')
         return v
-        
+
     def __checksum(self, ba):
         """XOR checksum of bytearray."""
         csum = 0
         for c in ba:
             csum ^= c
         return csum
-        
+
     def __storeLocation(self):
-        """Generator for the two storage locations."""
+        """Generate the two storage locations."""
         while True:
             yield 1
             yield 1+1+self.storeSize
